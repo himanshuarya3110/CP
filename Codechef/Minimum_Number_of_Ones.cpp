9252795be7334204fcb932d6665eb731574c4d88
Himanshu Arya <85223926+himanshuarya3110@users.noreply.github.com>
2022-11-30 22:20:44 +0530

    Create Minimum_Number_of_Ones.cpp

---
+++ Codechef/Minimum_Number_of_Ones.cpp
@@ -0,0 +1,18 @@
+// Minimum Number of Ones
+// https://www.codechef.com/START67D/problems/MINNOO
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cout<<n/2<<endl;
+	}
+	return 0;
+}
